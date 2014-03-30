e1d6f2f73bdf772870f47e13b107c8c76601dbdb
unknown <trohinde@NOOSLTROHINDE.crayon.as>
2014-03-30 19:14:15 +0200

    added testrunbook.ps1

---
+++ TestRunbook.ps1
@@ -0,0 +1,7 @@
+workflow Testrunbook
+{    
+    [string] $SampleMessage = Get-AutomationVariable -Name 'SampleMessage'
+
+    # Prints out 'SampleMessage' 'NumberOfIterations' times.
+        Write-Output "$SampleMessage"
+}
